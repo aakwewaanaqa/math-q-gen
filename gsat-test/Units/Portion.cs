8402864c29a8169dd9a654e2616f2d872d989443
小馬 <vvbdac58@gmail.com>
2025-01-03 18:04:42 +0800

    新增比、比值、成正比。

---
+++ gsat-test/Units/Portion.cs
@@ -0,0 +1,13 @@
+﻿using Gsat.Core.Structs;
+
+namespace Gsat.Units;
+
+[QuestionMaker("math", "比、比值、正比", 6)]
+public class Portion
+{
+    // [Question(1)]
+    // public Func<Question> _1 => () =>
+    // {
+    //
+    // };
+}
